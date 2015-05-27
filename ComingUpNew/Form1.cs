3c378b03bb93ade4e25efac153c816352631577f
mjpournassari <mjpournassari@gmail.com>
2015-05-27 07:08:19 +0430

    * Time DD

--- ComingUpNew/Form1.cs
+++ ComingUpNew/Form1.cs
@@ -123,7 +123,7 @@ namespace ComingUp
                 DateTime ProgTime = DateTime.Parse(Sch_Dt.Rows[i]["datetime"].ToString());
                 //Program1 = ["The World after Fukushima 2","01:00"]
 
-                string ProgTimeText = ProgTime.ToShortTimeString().Substring(0, 1) + ":" + ConfigurationSettings.AppSettings["TimeScheduleMinute"].ToString();
+                string ProgTimeText = ProgTime.Hour.ToString("00") + ":" + ConfigurationSettings.AppSettings["TimeScheduleMinute"].ToString();
                 Data.AppendLine("Program" + (i + 1).ToString() + " = [\"" + ProgName + "\",\"" + ProgTimeText + "\"]");
 
             }
